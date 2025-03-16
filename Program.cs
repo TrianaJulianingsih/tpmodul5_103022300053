26b29b2e887f44779ef5d44a55b88f44a513ebb2
IntanNurAini <intan.nuraini2005@gmail.com>
2025-03-16 21:03:04 +0700

    menambahkan class DataGeneric

--- Program.cs
+++ Program.cs
@@ -1,1 +1,28 @@
-﻿//
+﻿
+
+using System;
+
+public class DataGeneric<T>
+{
+    private T data;
+
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine($"Data yang tersimpan adalah: {data}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        DataGeneric<string> nimData = new DataGeneric<string>("103022300053");
+
+        nimData.PrintData();
+    }
+}
